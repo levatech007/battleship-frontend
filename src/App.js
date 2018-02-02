f11d75a5cb2c993db4b4f8d8ac3d7f760f4e1d7c
Eva <evaliisagalen@gmail.com>
2018-02-02 08:46:56 -0800

    installed bootstrap, created scalable test grid with map

--- src/App.js
+++ src/App.js
@@ -3,15 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.onClick = this.onClick.bind(this)
+  }
+
+  onClick(row, column) {
+    console.log("Square clicked!")
+  }
+
   render() {
+    let gameBoard = new Array()
+    let gameRow = new Array(10);
+    gameRow.fill(0)
+    for(let i = 0; i < gameRow.length; i++) {
+      gameBoard.push(gameRow)
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Battleship game coming soon</h1>
-        </header>
+      <div className='container'>
+            {
+              gameBoard.map((oneRow, rowIdx) => {
+                return( <div className='row justify-content-md-center'>
+                  {
+                    oneRow.map((oneSquare, colIdx) => {
+                      return( <div className='col-1 square' onClick={ () => this.onClick(rowIdx, colIdx) }></div> )
+                    })
+                  }
+                </div>)
+              })
+            }
       </div>
     );
   }
+
 }
 
 export default App;
