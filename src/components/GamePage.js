5549f7b9966080f6881a02bbadefe77115c911e1
Kyle Easton <kyleeaston@Kyles-MacBook-Air.local>
2018-02-02 16:58:21 -0800

    opponent board swaps in

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -9,13 +9,8 @@ class GamePage extends Component {
       allGuesses: ["A1", "A3", "B3", "C3", "B1", "C1", "D6", "E6", "D7", "G7", "A2", "B2"],
       clickedStartGame: false
     }
-    this.onClick = this.onClick.bind(this);
     this.hasClickedToPlay = this.hasClickedToPlay.bind(this);
 
-  }
-
-  onClick(row, column) {
-    console.log(`Clicked row ${row}, column ${column}`)
   }
 
   hasClickedToPlay() {
@@ -26,20 +21,8 @@ class GamePage extends Component {
 
   render() {
 
-    let gameBoard = new Array()
-    let gameRow = new Array(10);
-    gameRow.fill(0)
-    for(let i = 0; i < gameRow.length; i++) {
-      gameBoard.push(gameRow)
-    }
-
-    // const gameStarted = this.state.clickedStartGame
-    // if (gameStarted) {
-    //   return (
-    //     <Grid />
-    //   )
-    // }
-
+    const gameStarted = this.state.clickedStartGame;
+     
     return (
       <div className="container">
         <div className="row">
@@ -50,27 +33,21 @@ class GamePage extends Component {
         <div className="row">
           <div className="col-md-6">
             <h2>Your gameboard</h2>
-            <div id="your-gameboard" className="gameboard">
-                {gameBoard.map((oneRow, rowIdx) => {
-                  return( <div className='row justify-content-md-center'>
-                    {
-                      oneRow.map((oneSquare, colIdx) => {
-                        return( <div className='col-1 square' onClick={ () => this.onClick(rowIdx, colIdx) }></div> )
-                      })
-                    }
-                    </div>)
-                  })
-                }
+            <Grid />
+          </div>
+          {gameStarted ? (
+            <div className="col-md-6">
+              <h2>Sink your enemy</h2>
+              <Grid />
             </div>
-          </div>
-          <div className="col-md-6">
-            <h2>Place your battleships!</h2>
-              <p>Click the squares on your board to place your ships!</p>
-              <button className="btn btn-outline-success" onClick={this.hasClickedToPlay}>Start Game</button>
-          </div>
-        </div>
+            ) : (
+            <div className="col-md-6">
+              <h2>Place your battleships!</h2>
+                <h5>Click the squares on your board to place your ships then click 'Start Game'</h5>
+                <button className="btn btn-outline-success" onClick={ this.hasClickedToPlay }>Start Game</button>
+            </div>
+          )}
         </div> 
-        <Grid />
 
         <div className="row">
           <div className="col-md-6">
@@ -101,22 +78,8 @@ class GamePage extends Component {
         <Link to={ '/' } className="btn btn-outline-info btn-lg">Return Home</Link>
         <Link to={ '/' } className="btn btn-outline-danger btn-lg">Quit Game</Link>
       </div>
-      )
+    )
   }
 }
-// opponent game board
-// <h2>Sink your opponent</h2>
-// <div id="your-gameboard" className="gameboard">
-  // {gameBoard.map((oneRow, rowIdx) => {
-    // return( <div className='row justify-content-md-center'>
-    // {
-      // oneRow.map((oneSquare, colIdx) => {
-        // return( <div className='col-1 square' onClick={ () => this.onClick(rowIdx, colIdx) }></div> )
-      // })
-    // }
-    // </div>)
-  // })
-// }
-// </div>
 
 export default GamePage
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,34 +1,37 @@
 import React, { Component } from 'react';
 
 class Grid extends Component {
+  constructor() {
+    super();
+    this.onBoxClick = this.onBoxClick.bind(this);
+  }
+
+  onBoxClick(row, column) {
+    console.log(`Clicked row ${row}, column ${column}`)
+  }
+
   render() {
-	let gameBoard = new Array()
-	let gameRow = new Array(10);
-	gameRow.fill(0)
-	for(let i = 0; i < gameRow.length; i++) {
-		gameBoard.push(gameRow)
+
+  	let gameBoard = new Array()
+  	let gameRow = new Array(10);
+  	gameRow.fill(0)
+  	for(let i = 0; i < gameRow.length; i++) {
+  		gameBoard.push(gameRow)
     }
+
     return (
-    	<div className="row">
-    	<div className="col-md-6">
-    	<h1>adfads</h1>
-    	</div>
-		  <div className="col-md-6">
-		    <h2>Sink your opponent</h2>
-		    <div id="your-gameboard" className="gameboard">
-		        {gameBoard.map((oneRow, rowIdx) => {
-		          return( <div className='row justify-content-md-center'>
-		            {
-		              oneRow.map((oneSquare, colIdx) => {
-		                return( <div className='col-1 square' onClick={ () => this.onClick(rowIdx, colIdx) }></div> )
-		              })
-		            }
-		            </div>)
-		          })
-		        }
-		    </div>
-		  </div>
-		 </div> 
+	    <div id="your-gameboard" className="gameboard">
+	        {gameBoard.map((oneRow, rowIdx) => {
+	          return( <div className='row justify-content-md-center'>
+	            {
+	              oneRow.map((oneSquare, colIdx) => {
+	                return( <div className='col-1 square' onClick={ () => this.onBoxClick(rowIdx, colIdx) }></div> )
+	              })
+	            }
+	            </div>)
+	          })
+	        }
+	    </div>
     );
   }
 }
